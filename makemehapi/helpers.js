c5f316fd44f4db64b5d7c33a90ad322747f94b6a
Robbie Holmes <robbiethegeek@gmail.com>
2015-08-23 12:06:34 -0400

    Moving over the work I did locally on makemehapi

---
+++ makemehapi/helpers.js
@@ -0,0 +1,7 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
---
+++ makemehapi/views.js
@@ -0,0 +1,26 @@
+var Hapi = require('hapi');
+var path = require('path');
+
+var server = new Hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+	path: '/',
+	method: 'GET',
+	handler: {
+		view: 'index.html'
+	}
+});
+
+server.views({
+	engines: {
+		html: require('handlebars')
+	},
+	path: path.join(__dirname, 'templates')
+});
+
+server.start();
