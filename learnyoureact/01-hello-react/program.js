2d429e1eed021f4e30f9565da81813d4f319e003
Robbie Holmes <robbiethegeek@gmail.com>
2016-03-18 22:29:15 -0400

    Adding learnyoureact changes

--- learnyoureact/01-hello-react/program.js
+++ learnyoureact/01-hello-react/program.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var app = express();
+    var app = express();
 
-app.set('port', (process.argv[2] || 3000));
-app.set('view engine', 'jsx');
-app.set('views', __dirname + '/views');
-app.engine('jsx', require('express-react-views').createEngine());
+    app.set('port', (process.argv[2] || 3000));
+    app.set('view engine', 'jsx');
+    app.set('views', __dirname + '/views');
+    app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
 
-require('node-jsx').install();
+    require('babel/register')({
+        ignore: false
+    });
 
-app.use('/', function(req, res) {
-  res.render('index', '');
-});
+    app.use('/', function(req, res) {
+      res.render('index', '');
+    });
 
-app.listen(app.get('port'), function() {});
+    app.listen(app.get('port'), function() {});
